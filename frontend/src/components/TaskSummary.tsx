0b5473e2ae316c8572e2234c4e4eef1c0aaabe37
Tuttu13 <tutui0426@yahoo.co.jp>
2024-07-07 08:16:28 +0900

    TODO一覧表示　タスク件数　完了件数　修正

--- frontend/src/components/TaskSummary.tsx
+++ frontend/src/components/TaskSummary.tsx
@@ -1,30 +1,15 @@
 import { Box, Typography } from "@mui/material";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-// APIのベースURL
-const API_BASE_URL = "http://localhost:8000/api/todo/";
+interface TaskSummaryProps {
+  totalTasks: number;
+  completedTasks: number;
+}
 
-interface TaskSummaryProps {}
-
-const TaskSummary: React.FC<TaskSummaryProps> = () => {
-  const [totalTasks, setTotalTasks] = useState<number>(0);
-  const [completedTasks, setCompletedTasks] = useState<number>(0);
-
-  useEffect(() => {
-    const fetchTaskSummary = async () => {
-      try {
-        const response = await axios.get(`${API_BASE_URL}summary/`);
-        setTotalTasks(response.data.total_tasks);
-        setCompletedTasks(response.data.completed_tasks);
-      } catch (error) {
-        console.error("Error fetching task summary:", error);
-      }
-    };
-
-    fetchTaskSummary();
-  }, []);
-
+const TaskSummary: React.FC<TaskSummaryProps> = ({
+  totalTasks,
+  completedTasks,
+}) => {
   return (
     <Box sx={{ textAlign: "center", mb: 2 }}>
       <Typography variant="h6">タスク件数: {totalTasks}</Typography>
