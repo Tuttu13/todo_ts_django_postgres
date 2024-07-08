5d5b2e3c932f38450f380e0cc22e70308a9f5b31
Tuttu13 <tutui0426@yahoo.co.jp>
2024-07-09 07:43:04 +0900

    バックエンド　urls.pyのname属性の名称修正

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("todo/", views.ListView.as_view(), name="list"),
-    path("todo/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("todo/", views.ListView.as_view(), name="task-list"),
+    path("todo/<int:pk>/", views.DetailView.as_view(), name="task-detail"),
     path('todo/summary/', views.task_summary, name='task-summary'),
 ]
